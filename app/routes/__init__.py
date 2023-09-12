ce2bac0ba3b6d45b7e56e32c8d44ecc6324d9585
AlexandreDev06 <alexandre.macedo@ringa.com.br>
2023-09-12 15:54:59 -0300

    Created structure for archives to run celery jobs

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter
 
 from .clients import router as router_clients
 from .health import router as router_health
+from .motor_runnings import router as router_motor_runnings
 from .operations import router as router_operations
 
 routers = APIRouter(prefix="/api/v1")
 
+routers.include_router(router_motor_runnings)
 routers.include_router(router_operations)
 routers.include_router(router_clients)
 routers.include_router(router_health)
